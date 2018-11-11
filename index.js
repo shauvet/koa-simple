35fe267937106ef6509c1d412c0eab758da96761
yangxiaowei <wangzhe23@gmail.com>
2018-11-11 19:40:14 +0800

    add context compose

--- index.js
+++ index.js
@@ -1,9 +1,22 @@
-let Koa = require('./koa/application');
+let Koa = require('./src/application');
 let app = new Koa();
 
-app.use((req, res) => {
-  res.writeHead(200);
-  res.end('hello world');
+app.use(async (ctx, next) => {
+  console.log(1);
+  await next();
+  console.log(6);
+});
+
+app.use(async (ctx, next) => {
+  console.log(2);
+  await next();
+  console.log(5);
+});
+
+app.use(async (ctx, next) => {
+  console.log(3);
+  ctx.body = "hello world";
+  console.log(4);
 });
 
 app.listen(3000, () => {
---
+++ src/application.js
@@ -0,0 +1,54 @@
+let http = require('http');
+let context = require('./context');
+let request = require('./request');
+let response = require('./response');
+
+class Application {
+  constructor() {
+    this.callbackFunc;
+  }
+  listen(port) {
+    let server = http.createServer(this.callback());
+    server.listen(port);
+  }
+  use(fn) {
+    this.callbackFunc = fn;
+  }
+  createContext(req, res) {
+    let ctx = Object.create(this.context);
+    ctx.request = Object.create(this.request);
+    ctx.response = Object.create(this.response);
+    ctx.req = ctx.request.req = req;
+    ctx.res = ctx.response.res = res;
+    return ctx;
+  }
+  compose() {
+    return async ctx => {
+      function createNext(middleware, oldNext) {
+        return async () => {
+          await middleware(ctx, oldNext);
+        }
+      }
+      let len = this.middlewares.length;
+      let next = async () => {
+        return Promise.resolve();
+      }
+      for (let i = len - 1; i >= 0; i--) {
+        let currentMiddleWare = this.middlewares[i];
+        next = createNext(currentMiddleWare, next);
+      }
+      await next();
+    }
+  }
+  callback() {
+    return (req, res) => {
+      let ctx = this.createContext(req, res);
+      let respond = () => this.responseBody(ctx);
+      let onerror = err => this.onerror(err, ctx);
+      let fn = this.compose();
+      return fn(ctx);
+    }
+  }
+}
+
+module.exports = Application;
